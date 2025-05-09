9bcc27f02e2664a8412260f89e33b0934d33fc9d
Felipe Llanos <felipellanos515@gmail.com>
2025-05-09 20:59:01 -0300

    Intercambio: se reutiliza la lógica de productos en la App Personas

--- App Personas/index.js
+++ App Personas/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const personaControlador = require('./Presentacion/personaControlador');
+const productoControlador = require('./Presentacion/productoControlador');
 
 const app = express();
 app.use(express.json());
 
 app.use('/personas', personaControlador);
+app.use('/productos', productoControlador);
 
 const PORT = 3000;
 app.listen(PORT, () => {
